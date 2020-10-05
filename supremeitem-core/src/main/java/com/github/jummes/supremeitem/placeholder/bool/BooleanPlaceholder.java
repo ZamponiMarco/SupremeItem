f45a1cda777d40dece84c0094a792bba4d195193
jummes <32539635+ZamponiMarco@users.noreply.github.com>
2020-10-05 18:20:23 +0200

    [UPDATE] Added is sneaking placeholder
    Added a placeholder that checks if the source or the target of a skill is sneaking, this can be used inside your boolean conditions to enhance the possibilities of your items, more placeholders are to come

--- supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/bool/BooleanPlaceholder.java
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/bool/BooleanPlaceholder.java
@@ -3,7 +3,7 @@ package com.github.jummes.supremeitem.placeholder.bool;
 import com.github.jummes.libs.annotation.Enumerable;
 import com.github.jummes.supremeitem.placeholder.Placeholder;
 
-@Enumerable.Parent(classArray = {OnGroundPlaceholder.class, PossessItemPlaceholder.class})
+@Enumerable.Parent(classArray = {OnGroundPlaceholder.class, PossessItemPlaceholder.class, IsSneakingPlaceholder.class})
 public abstract class BooleanPlaceholder extends Placeholder<Boolean> {
     public BooleanPlaceholder(boolean target) {
         super(target);
---
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/bool/IsSneakingPlaceholder.java
@@ -0,0 +1,49 @@
+package com.github.jummes.supremeitem.placeholder.bool;
+
+import com.github.jummes.libs.annotation.Enumerable;
+import com.github.jummes.supremeitem.action.source.EntitySource;
+import com.github.jummes.supremeitem.action.source.Source;
+import com.github.jummes.supremeitem.action.targeter.EntityTarget;
+import com.github.jummes.supremeitem.action.targeter.Target;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+@Enumerable.Child
+@Enumerable.Displayable(name = "&c&lIs Sneaking Placeholder", description = "gui.placeholder.is-sneaking.description", headTexture = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTQyMzI5YTljNDEwNDA4NDE5N2JkNjg4NjE1ODUzOTg0ZDM3ZTE3YzJkZDIzZTNlNDEyZGQ0MmQ3OGI5OGViIn19fQ==")
+public class IsSneakingPlaceholder extends BooleanPlaceholder {
+    public IsSneakingPlaceholder() {
+        this(TARGET_DEFAULT);
+    }
+
+    public IsSneakingPlaceholder(boolean target) {
+        super(target);
+    }
+
+    public static IsSneakingPlaceholder deserialize(Map<String, Object> map) {
+        boolean target = (boolean) map.get("target");
+        return new IsSneakingPlaceholder(target);
+    }
+
+    @Override
+    public Boolean computePlaceholder(Target target, Source source) {
+        if (this.target) {
+            if (target instanceof EntityTarget) {
+                LivingEntity livingEntity = ((EntityTarget) target).getTarget();
+                if (livingEntity instanceof Player) {
+                    Player player = (Player) livingEntity;
+                    return player.isSneaking();
+                }
+            }
+        }
+        if (source instanceof EntitySource) {
+            LivingEntity livingEntity = ((EntitySource) source).getSource();
+            if (livingEntity instanceof Player) {
+                Player player = (Player) livingEntity;
+                return player.isSneaking();
+            }
+        }
+        return false;
+    }
+}
