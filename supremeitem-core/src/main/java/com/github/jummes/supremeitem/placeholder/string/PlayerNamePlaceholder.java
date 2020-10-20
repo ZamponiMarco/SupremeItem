7e1c106bc9e6e91faaad9313cd13c9382658f776
ZamponiMarco <zamponi.marco3@gmail.com>
2020-10-21 00:13:35 +0200

    [UPDATE] Added Player Name Placeholder
    Added PlayerNamePlaceholder, the first String placeholder, it's main function for now is to make some tests but meanwhile it's available for use by everybody (p.s. can be used in CommandAction)

---
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/string/PlayerNamePlaceholder.java
@@ -0,0 +1,58 @@
+package com.github.jummes.supremeitem.placeholder.string;
+
+import com.github.jummes.libs.annotation.Enumerable;
+import com.github.jummes.supremeitem.action.source.EntitySource;
+import com.github.jummes.supremeitem.action.source.Source;
+import com.github.jummes.supremeitem.action.targeter.EntityTarget;
+import com.github.jummes.supremeitem.action.targeter.Target;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+@Enumerable.Child
+public class PlayerNamePlaceholder extends StringPlaceholder {
+    public PlayerNamePlaceholder(boolean target) {
+        super(target);
+    }
+
+    public PlayerNamePlaceholder() {
+        this(TARGET_DEFAULT);
+    }
+
+    public static PlayerNamePlaceholder deserialize(Map<String, Object> map) {
+        boolean target = (boolean) map.getOrDefault("target", TARGET_DEFAULT);
+        return new PlayerNamePlaceholder(target);
+    }
+
+    @Override
+    public StringPlaceholder clone() {
+        return new PlayerNamePlaceholder(target);
+    }
+
+    @Override
+    public String computePlaceholder(Target target, Source source) {
+        if (this.target) {
+            if (target instanceof EntityTarget) {
+                LivingEntity livingEntity = ((EntityTarget) target).getTarget();
+                if (livingEntity instanceof Player) {
+                    Player player = (Player) livingEntity;
+                    return player.getName();
+                }
+            }
+        }
+        if (source instanceof EntitySource) {
+            LivingEntity livingEntity = ((EntitySource) source).getSource();
+            if (livingEntity instanceof Player) {
+                Player player = (Player) livingEntity;
+                return player.getName();
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public String getName() {
+        return String.format("%s Player Name", target ? "Target" : "Source");
+    }
+}
---
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/string/StringPlaceholder.java
@@ -0,0 +1,13 @@
+package com.github.jummes.supremeitem.placeholder.string;
+
+import com.github.jummes.libs.annotation.Enumerable;
+import com.github.jummes.supremeitem.placeholder.Placeholder;
+
+@Enumerable.Parent(classArray = {PlayerNamePlaceholder.class})
+public abstract class StringPlaceholder extends Placeholder<String> {
+    public StringPlaceholder(boolean target) {
+        super(target);
+    }
+
+    public abstract StringPlaceholder clone();
+}
