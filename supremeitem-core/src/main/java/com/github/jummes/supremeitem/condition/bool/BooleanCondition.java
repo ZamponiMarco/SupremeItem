c7c8f25b8f0dd947325a1c1e61c8842fd6894355
ZamponiMarco <32539635+ZamponiMarco@users.noreply.github.com>
2021-02-04 22:53:32 +0100

    [UPDATE] Added missing value tooltip in BooleanCondition

--- supremeitem-core/src/main/java/com/github/jummes/supremeitem/condition/bool/BooleanCondition.java
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/condition/bool/BooleanCondition.java
@@ -18,7 +18,8 @@ public class BooleanCondition extends TrueFalseCondition {
 
     private static final String PLACEHOLDER_HEAD = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmYzOGQ2MTIwM2U0ZTZhMmM5MjQ4MDMwNjA5ZWZiNTc3YjRmZTllZjc4NmNhNDBkYjk0M2Y1M2Y0ODBhZTY4OCJ9fX0=";
 
-    @Serializable(headTexture = PLACEHOLDER_HEAD, description = "gui.condition.boolean.value")
+    @Serializable(headTexture = PLACEHOLDER_HEAD, description = "gui.condition.boolean.value",
+            additionalDescription = {"gui.additional-tooltips.value"})
     private BooleanPlaceholder value;
 
     public BooleanCondition() {
