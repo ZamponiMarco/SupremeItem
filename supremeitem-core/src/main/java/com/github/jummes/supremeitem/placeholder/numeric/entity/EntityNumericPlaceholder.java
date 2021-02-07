24ba8ea2f42096f476eeafe2a80249715283e747
ZamponiMarco <32539635+ZamponiMarco@users.noreply.github.com>
2021-02-07 18:37:09 +0100

    [UPDATE] Added entity height placeholder

--- supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/numeric/entity/EntityNumericPlaceholder.java
+++ supremeitem-core/src/main/java/com/github/jummes/supremeitem/placeholder/numeric/entity/EntityNumericPlaceholder.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @Enumerable.Parent(classArray = {AttributeNumericPlaceholder.class, PlayerNumericPlaceholder.class, HealthPlaceholder.class,
         MaxHealthPlaceholder.class, NumericVariablePlaceholder.class, ArmorPlaceholder.class, SpeedPlaceholder.class,
-        KnockbackResistancePlaceholder.class, WidthPlaceholder.class})
+        KnockbackResistancePlaceholder.class, WidthPlaceholder.class, HeightPlaceholder.class})
 @Enumerable.Displayable(name = "&c&lEntity Placeholders", description = "gui.placeholder.double.entity.description", headTexture = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTZmYzg1NGJiODRjZjRiNzY5NzI5Nzk3M2UwMmI3OWJjMTA2OTg0NjBiNTFhNjM5YzYwZTVlNDE3NzM0ZTExIn19fQ==")
 public abstract class EntityNumericPlaceholder extends NumericPlaceholder {
 
